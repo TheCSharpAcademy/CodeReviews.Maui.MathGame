71e8e3a4bfbb8a12dc4f9ba0af3a3e950972fb56
maccer989 <132687553+maccer989@users.noreply.github.com>
2023-09-02 14:49:23 +0100

    Project Completed

---
+++ MathGame.Maui.maccer989/MathGame.Maui/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MathGame.Maui.Data;
+
+namespace MathGame.Maui;
+
+public partial class App : Application
+{
+	public static GameRespository GameRespository { get; private set; }
+	public App(GameRespository gameRespository)
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+
+		GameRespository = gameRespository;
+	}
+}
