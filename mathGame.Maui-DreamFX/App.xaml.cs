c523af5218200737ddf2e7f92c17c25187f6cd19
DreamFXX <filipek.husak@gmail.com>
2024-11-11 00:10:15 +0100

    moved my mathGame project to review repository

---
+++ mathGame.Maui-DreamFX/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using mathGame.Maui.Data;
+
+namespace mathGame.Maui
+{
+    public partial class App : Application
+    {
+        // Injekction dependency - for whole app, App.cs
+        public static GameRepository GameRepository { get; private set; }
+
+
+        public App(GameRepository gameRepository)
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+            GameRepository = gameRepository;
+        }
+    }
+}
---
+++ mathGame.Maui-DreamFX/Models/Game.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
+
+namespace mathGame.Maui.Models
+{
+    // SQLite
+    [Table("game")]
+    public class Game
+    {
+        // Vytvořena Models složka pro storovaní dat pomocí SQL.
+
+
+        // Příkaz pro SQL - refaktoring - nainstalovat nabízený balíček.
+        [PrimaryKey, AutoIncrement, Column("Id")]
+
+        // Setting properties (Vlastnosti) databáze ve které se budou ukládat data.
+        public int Id { get; set; }
+        public GameOperation Type { get; set; }
+        public int Score { get; set; }
+        public DateTime DatePlayed { get; set; }
+
+    }
+
+    // Název operace - ENUMERACE
+    // Typy pro akci GameOperation, na výběr.
+    public enum GameOperation
+    {
+        Sčítání,
+        Odčítání,
+        Násobení,
+        Dělení,
+    }
+}
